ebe6a20de2ffca671acd883a6aef85d8d096fd82
Alanazi <alanaziibasel@gmail.com>
2025-03-20 11:13:58 +0300

    feat: rate limit

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'v1'], function ($router) {
+Route::group(['prefix' => 'v1', 'middleware' => 'throttle:api'], function ($router) {
     Route::group(['prefix' => 'auth'], function ($router) {
         Route::post('/register', [AuthController::class, 'register']);
     });
